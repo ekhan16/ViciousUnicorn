ea7e2cf4afb3ce89c93363d1e22a19a018bd6cd7
David Trinh <dhtrinh02@gmail.com>
2015-09-28 14:47:37 -0700

    (feat) Implement basic ng-repeat logic within Home view and HomeCtrl to load dummy story data.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,9 @@
 angular.module('ViciousUnicorn.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope) {
+  $scope.chapters = [{sections: 1, content:'One day, Ekhan went to the mall.'},
+  {sections: 2,content:'Two days latter he got married to a woman.'},
+  {sections: 3, content:'Three years later, he was so whipped by his womean, he barely left the house.'}];
+})
 
 .controller('SettingsCtrl', function($scope) {});
